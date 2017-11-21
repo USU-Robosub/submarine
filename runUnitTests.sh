1f4ec5633ff639fafc9c7e6df9ea30aff2b45ec9
Konnor Andrews <konnordandrews@gmail.com>
2017-11-21 15:41:03 -0700

    javascript unit test added

--- runUnitTests.sh
+++ runUnitTests.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# Utils - C++
 cd utils/cpp
 mkdir build
 cd build
 cmake .. -Dtests=ON
 make
 ./UtilsTests
+cd ../../..
+# Utils - Javascript
+cd utils/javascript
+npm run test
+./UtilsTests
+cd ../..
