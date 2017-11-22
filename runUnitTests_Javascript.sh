c1b1edaff75fa6f3b48233d6eba680fe74ad5c1c
Konnor Andrews <konnordandrews@gmail.com>
2017-11-21 18:14:38 -0700

    path fix maybe

--- runUnitTests_Javascript.sh
+++ runUnitTests_Javascript.sh
@@ -6,6 +6,4 @@ npm install
 # npm run test
 cd ../..
 node utils/javascript/node_modules/jest/bin/jest.js --config utils/javascript/config.json --coverage
-cd utils/javascript
-cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
-cd ../..
+cat utils/javascript/coverage/lcov.info | utils/javascript/node_modules/coveralls/bin/coveralls.js
