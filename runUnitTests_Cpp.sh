672cbca713d24afcc03d47653f1d5c31113b65bd
Konnor Andrews <konnordandrews@gmail.com>
2017-11-21 18:37:51 -0700

    atempt at excluding test files and cmake files

--- runUnitTests_Cpp.sh
+++ runUnitTests_Cpp.sh
@@ -8,4 +8,4 @@ cmake .. -Dtests=ON -Dcoverage=ON
 make
 ./UtilsTests
 cd ../../..
-coveralls -i utils/cpp -e utils/cpp/build/CMakeFiles/feature_tests.cxx -e utils/cpp/build/CMakeFiles/feature_tests.c
+coveralls -i utils/cpp -E .+\/(feature_tests|.+\.spec|CMake(C|CXX)CompilerId).+
