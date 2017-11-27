92715e851ad9e7e35534c96b559bb22704c2dfb8
Konnor Andrews <konnordandrews@gmail.com>
2017-11-27 09:53:02 -0700

    removed mocks from cpp coverage report

--- bin/runUnitTests_Cpp.sh
+++ bin/runUnitTests_Cpp.sh
@@ -8,4 +8,4 @@ cmake .. -Dtests=ON -Dcoverage=ON
 make
 ./Utils_Tests
 cd ../../..
-coveralls -i utils/cpp -E ".+\/(feature_tests|.+\.spec|CMake(C|CXX)CompilerId|test\/main).+"
+coveralls -i utils/cpp -E ".+\/(feature_tests|.+\.spec|CMake(C|CXX)CompilerId|test\/main|.+\.mock).+"
