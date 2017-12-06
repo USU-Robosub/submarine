8b1f0411323435680b9e4b285c5a89934d49f0da
Konnor Andrews <konnordandrews@gmail.com>
2017-12-05 17:50:07 -0700

    improved thruster tests

--- utils/cpp/src/Device/Thruster.spec.cpp
+++ utils/cpp/src/Device/Thruster.spec.cpp
@@ -7,7 +7,7 @@ TEST_CASE("thruster sends power set to hub as integer", "[Thruster]"){
   Comm::TestHub hub;
   Device::Thruster thruster(&hub, 0);
   thruster.enable();
-  
+
   thruster.setPower(0.5);
   REQUIRE( hub.writeQueue[1] == 300 ); // 0 - 200 - 400
 }
@@ -15,22 +15,27 @@ TEST_CASE("thruster sends power set to hub as integer", "[Thruster]"){
 TEST_CASE("thruster implements the motor interface", "[Thruster]"){
   Comm::TestHub hub;
   Device::Thruster thruster(&hub, 0);
+  Device::Motor* motor = &thruster;
   thruster.enable();
 
-  Device::Motor* motor = &thruster;
   motor->setPower(-0.1);
   REQUIRE( hub.writeQueue[1] == 180 );
 }
 
-TEST_CASE("thruster limits power to -1 <= x => 1", "[Thruster]"){
+TEST_CASE("thruster limits power to -1 <= x <= 1", "[Thruster]"){
   Comm::TestHub hub;
   Device::Thruster thruster(&hub, 0);
   thruster.enable();
 
-  thruster.setPower(-1.5);
-  REQUIRE( hub.writeQueue[1] == 0 );
-  thruster.setPower(1.2);
-  REQUIRE( hub.writeQueue[3] == 400 );
+  SECTION("limits to x => -1"){
+    thruster.setPower(-1.5);
+    REQUIRE( hub.writeQueue[1] == 0 );
+  }
+
+  SECTION("limits to x => 1"){
+    thruster.setPower(1.2);
+    REQUIRE( hub.writeQueue[1] == 400 );
+  }
 }
 
 TEST_CASE("thruster locks hub when set power", "[Thruster]"){
@@ -38,11 +43,17 @@ TEST_CASE("thruster locks hub when set power", "[Thruster]"){
   Device::Thruster thruster(&hub, 0);
   thruster.enable();
 
-  REQUIRE( hub.didLock == false );
-  REQUIRE( hub.didUnlock == false );
-  thruster.setPower(0.3);
-  REQUIRE( hub.didLock == true );
-  REQUIRE( hub.didUnlock == true );
+  SECTION("locks hub"){
+    REQUIRE( hub.didLock == false );
+    thruster.setPower(0.3);
+    REQUIRE( hub.didLock == true );
+  }
+
+  SECTION("unlocks hub"){
+    REQUIRE( hub.didLock == false );
+    thruster.setPower(0.3);
+    REQUIRE( hub.didUnlock == true );
+  }
 }
 
 TEST_CASE("thruster sends id of thruster to set power of", "[Thruster]"){
