85325f8ae6be1fffa76ae08cef409e5adabf4b75
Konnor Andrews <konnordandrews@gmail.com>
2017-12-07 01:14:13 -0700

    thruster and motor changed to reactive style

--- utils/cpp/include/Device/Thruster.h
+++ utils/cpp/include/Device/Thruster.h
@@ -1,22 +1,15 @@
 #ifndef DEVICE_THRUSTER
 #define DEVICE_THRUSTER
 
-#include <algorithm>
-#include "Device/Motor.h"
-#include "Comm/Hub.h"
+#include <vector>
+#include "rx.hpp"
+
+using namespace rxcpp;
 
 namespace Device{
-  class Thruster : public Motor{
-  public:
-    Thruster(Comm::Hub* hub, unsigned int id) : hub(hub), id(id) {}
-    void setPower(double power);
-    void enable();
-    void disable();
-  private:
-    bool isEnabled = false;
-    Comm::Hub* hub;
-    unsigned int id;
-  };
+  namespace Thruster{
+    observable<std::vector<int>> getSerialCommands(observable<int> power, int thrusterID);
+  }
 }
 
 #endif
