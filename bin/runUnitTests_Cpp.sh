1969308facb5b0cc06804fbc5bf3267ad425a1a8
Konnor Andrews <konnordandrews@gmail.com>
2018-01-15 13:07:30 -0700

    removed dump

--- bin/runUnitTests_Cpp.sh
+++ bin/runUnitTests_Cpp.sh
@@ -44,6 +44,6 @@ cd ../../..
 
 echo Gathering coverage info.
 {
-coveralls --gcov-options '\-lp' -i ./ -E ".*\/(extern|feature_tests|.+\.spec|CMake(C|CXX)CompilerId|test\/main|.+\.mock).+" --dump="dump.json"
+coveralls --gcov-options '\-lp' -i ./ -E ".*\/(extern|feature_tests|.+\.spec|CMake(C|CXX)CompilerId|test\/main|.+\.mock).+"
 } &> /dev/null
 echo Coverage info uploaded.
