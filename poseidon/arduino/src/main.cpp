66b67253b3a3dd638085661b3ed28908d1f5aba4
Konnor Andrews <konnordandrews@gmail.com>
2018-01-15 10:55:51 -0700

    arduino servo test

--- poseidon/arduino/src/main.cpp
+++ poseidon/arduino/src/main.cpp
@@ -1,32 +1,52 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
 #include <Arduino.h>
+#include <Servo.h>
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
 #endif
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+#define LED_BUILTIN 9
+
+// void setup()
+// {
+//   // initialize LED digital pin as an output.
+//   pinMode(LED_BUILTIN, OUTPUT);
+// }
+//
+// void loop()
+// {
+//   // turn the LED on (HIGH is the voltage level)
+//   digitalWrite(LED_BUILTIN, HIGH);
+//
+//   // wait for a second
+//   delay(1000);
+//
+//   // turn the LED off by making the voltage LOW
+//   digitalWrite(LED_BUILTIN, LOW);
+//
+//    // wait for a second
+//   delay(1000);
+// }
+
+
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(1000);
+void loop() {
+  for (pos = 90; pos <= 170; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 170; pos >= 90; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
