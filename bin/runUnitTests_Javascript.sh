be32c32091b9f73f5a5ee8b92735dc42bca4232d
Konnor Andrews <konnordandrews@gmail.com>
2018-01-15 18:08:59 -0700

    removed old scripts

--- bin/runUnitTests_Javascript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-# Utils
-cd utils/javascript
-npm install
-# npm run test
-cd ../..
-node utils/javascript/node_modules/jest/bin/jest.js --config utils/javascript/config.json --coverage
-cat utils/javascript/coverage/lcov.info | utils/javascript/node_modules/coveralls/bin/coveralls.js
