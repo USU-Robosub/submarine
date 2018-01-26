89c8e65df193159c1b3b47ce1f38fb603b50cd43
Konnor Andrews <konnordandrews@gmail.com>
2018-01-25 21:55:53 -0700

    docs added

--- utils/cpp/include/Comm/Hub.hpp
+++ utils/cpp/include/Comm/Hub.hpp
@@ -18,19 +18,25 @@ namespace Comm{
   class Hub;
 }
 
+/**
+ * Sends data...
+ */
 template<class T>
 class Comm::Hub{
 public:
+  /** An enum type.
+   *  The documentation block cannot be put after the enum!
+   */
   Hub(Comm::Bridge<T>* bridge);
-  virtual void emit(T name, std::vector<T> message);
-  virtual Comm::HubBinding<T> on(T name, Comm::HandlerFunction<T> handler);
-  virtual void poll();
-  virtual void remove(Comm::HubBinding<T> binding);
+  virtual void emit(T name, std::vector<T> message); /**< enum value 1 */
+  virtual Comm::HubBinding<T> on(T name, Comm::HandlerFunction<T> handler); /**< enum value 1 */
+  virtual void poll(); /**< enum value 1 */
+  virtual void remove(Comm::HubBinding<T> binding); /**< enum value 1 */
 
 protected:
-  Bridge<T>* bridge;
-  std::map<T, std::map<unsigned int, Comm::HandlerFunction<T>>> handlers;
-  std::map<T, unsigned int> nextIDs;
+  Bridge<T>* bridge; /**< enum value 1 */
+  std::map<T, std::map<unsigned int, Comm::HandlerFunction<T>>> handlers; /**< enum value 1 */
+  std::map<T, unsigned int> nextIDs; /**< enum value 1 */
 };
 
 template<class T>
