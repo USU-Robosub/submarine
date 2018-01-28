60e0c8decb9b8929ef54bb118a9d792ccdf04387
Konnor Andrews <konnordandrews@gmail.com>
2018-01-28 15:54:56 -0700

    more doxygen config added

--- utils/cpp/include/Comm/Hub.hpp
+++ utils/cpp/include/Comm/Hub.hpp
@@ -1,3 +1,32 @@
+/**
+ * \class ExampleClass
+ *
+ * \ingroup PackageName
+ * (Note, this needs exactly one \defgroup somewhere)
+ *
+ * \brief Provide an example
+ *
+ * This class is meant as an example.  It is not useful by itself
+ * rather its usefulness is only a function of how much it helps
+ * the reader.  It is in a sense defined by the person who reads it
+ * and otherwise does not exist in any real form.
+ *
+ * \note Attempts at zen rarely work.
+ *
+ * \author (last to touch it) $Author: bv $
+ *
+ * \version $Revision: 1.5 $
+ *
+ * \date $Date: 2005/04/14 14:16:20 $
+ *
+ * Contact: bv@bnl.gov
+ *
+ * Created on: Wed Apr 13 18:39:37 2005
+ *
+ * $Id: doxygen-howto.html,v 1.5 2005/04/14 14:16:20 bv Exp $
+ *
+ */
+
 #ifndef COMM_HUB
 #define COMM_HUB
 
@@ -7,10 +36,26 @@
 #include <queue>
 #include <Comm/Bridge.hpp>
 
+/**
+* \brief Callback function type "foo"
+*
+* A longer description of foo.
+* \param a Description for a
+* \param b Description for b
+* \return Description for return value
+*/
 namespace Comm{
   template<class T>
   struct HubBinding;
 
+  /**
+  * \brief Callback function type "foo"
+  *
+  * A longer description of foo.
+  * \param a Description for a
+  * \param b Description for b
+  * \return Description for return value
+  */
   template<class T>
   using HandlerFunction = std::function<void(std::vector<T>)>;
 
@@ -18,31 +63,57 @@ namespace Comm{
   class Hub;
 }
 
-/**
- * Sends data...
- */
+/// Provides a standard interface to emit and listen to events.
 template<class T>
 class Comm::Hub{
 public:
-  /** An enum type.
-   *  The documentation block cannot be put after the enum!
-   */
+  /// Create a Hub that uses a Bridge for sending events.
   Hub(Comm::Bridge<T>* bridge);
-  virtual void emit(T name, std::vector<T> message); /**< enum value 1 */
-  virtual Comm::HubBinding<T> on(T name, Comm::HandlerFunction<T> handler); /**< enum value 1 */
-  virtual void poll(); /**< enum value 1 */
-  virtual void remove(Comm::HubBinding<T> binding); /**< enum value 1 */
+
+  /** \brief Emits an event.
+    * \param name an identifier for the event.
+    * \param message a vector of data to send.
+    *
+    * Emits an event using the underlying Bridge.
+    * The Hub's own listeners will not be notified.
+    */
+  virtual void emit(T name, std::vector<T> message);
+
+  /** \brief Attaches a listener.
+    * \param name the identifier to listen for.
+    * \param handler callback of the form `void callback(vector<T>)`.
+    * \return The identifier of the listener created.
+    *
+    * Attaches a listener to the Hub.
+    * When an event with the given name is received, the handler will be called.
+    */
+  virtual Comm::HubBinding<T> on(T name, Comm::HandlerFunction<T> handler);
+
+  /** \brief Process events
+    *
+    * Get pending events from the Bridge.
+    * Then, for each event, notify the listeners associated with it.
+    */
+  virtual void poll();
+
+  /** \brief Removes a listener
+    * \param binding the listener's identifier
+    *
+    * Removes a listener from the Hub.
+    */
+  virtual void remove(Comm::HubBinding<T> binding);
 
 protected:
-  Bridge<T>* bridge; /**< enum value 1 */
-  std::map<T, std::map<unsigned int, Comm::HandlerFunction<T>>> handlers; /**< enum value 1 */
-  std::map<T, unsigned int> nextIDs; /**< enum value 1 */
+  Bridge<T>* bridge; ///< Underlying Bridge used for communication
+  std::map<T, std::map<unsigned int, Comm::HandlerFunction<T>>> handlers; ///< List of listener callbacks
+  std::map<T, unsigned int> nextIDs; ///< New listener ID tracking
 };
 
+/// Identifier for listeners attached to a Hub.
 template<class T>
 struct Comm::HubBinding{
-  T name;
-  unsigned int id;
+  T name; ///< Target event
+  unsigned int id; ///< Specific ID of listener
 };
 
 #include "../../src/Comm/Hub.ipp"
