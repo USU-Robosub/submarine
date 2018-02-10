4025692105ed71779cfe995cffc0f022a9eb19ae
Konnor Andrews <konnordandrews@gmail.com>
2018-02-09 20:25:35 -0700

    added joystick controls

--- poseidon/beaglebone/agent/index.js
+++ poseidon/beaglebone/agent/index.js
@@ -8,32 +8,34 @@ app.get('/', (req, res) => res.sendFile(path.resolve(__dirname + '/../../webapp/
 app.get('/socket.js', (req, res) => res.sendFile(path.resolve(__dirname + '/../../webapp/socketio/socket.io.min.js')))
 app.get('/rx.js', (req, res) => res.sendFile(path.resolve(__dirname + '/../../webapp/rxjs/rx.min.js')))
 app.get('/index.js', (req, res) => res.sendFile(path.resolve(__dirname + '/../../webapp/index.js')))
+app.get('/p5.js', (req, res) => res.sendFile(path.resolve(__dirname + '/../../webapp/p5js/p5.min.js')))
 
-comm.create({
-  address: 'localhost',
-  port: 3001,
-  separator: '|'
-}).then(hub => {
-  
+// comm.create({
+//   address: 'localhost',
+//   port: 3001,
+//   separator: '|'
+// }).then(hub => {
+
   io.on('connection', function(socket){
     console.log('a user connected')
     socket.on('disconnect', function(){
       console.log('user disconnected')
     })
     socket.on('throttle', function(x){
-      hub.emit('throttle', x)
+      console.log('throttle', x)
+      //hub.emit('throttle', x)
     })
     socket.on('steering', function(x){
-      console.log('user disconnected')
-      hub.emit('steering', x)
+      console.log('steering', x)
+      //hub.emit('steering', x)
     })
     socket.on('dive', function(x){
-      console.log('user disconnected')
-      hub.emit('dive', x)
+      console.log('dive', x)
+      //hub.emit('dive', x)
     })
   })
 
-  setInterval(hub.poll, 10)
-}).catch(error => console.log(error))
+//   setInterval(hub.poll, 10)
+// }).catch(error => console.log(error))
 
 http.listen(3000, () => console.log('Web app listening on port 3000'))
--- poseidon/webapp/index.js
+++ poseidon/webapp/index.js
@@ -1,37 +1,62 @@
 var socket = io();
 
-const gamepad = navigator.getGamepads()
+// const gamepad = navigator.getGamepads()
+
+let gamepadIndex = -1;
+
+console.log('app started')
+window.addEventListener("gamepadconnected", function(e) {
+  console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+    e.gamepad.index, e.gamepad.id,
+    e.gamepad.buttons.length, e.gamepad.axes.length);
+  gamepadIndex = e.gamepad.index
+});
+
+function setup() {
+  createCanvas(600, 300);
+}
+
+const pos = {x:0, y:0}
+const pos2 = {x:0, y:0}
+
+function draw() {
+  background(0);
+  noStroke();
+
+  fill(255);
+  ellipse(150 + pos.x,  150 + pos.y, 72, 72);
+  ellipse(450 + pos2.x,  150 + pos2.y, 72, 72);
+}
 
 var timer = Rx.Observable.timer(200, 10);
 
-function getValue(x){ return x.value; }
-function sameValue(a, b){ return Math.abs(a - b) < 0.01; }
-function checkAxis(name, x){ return x.axis == name; }
-function deadZone(x) { return Math.abs(x) < 0.2 ? 0 : x; }
-function modmod(x, n) { return x - Math.floor(x/n) * n; }
-function getAngle(a, b, n) { return modmod(a - b + n, n * 2) - n }
+const axis = index => () => navigator.getGamepads()[gamepadIndex].axes[index]
 
-var throttle = timer.map(function() {
-  return gamepad.gamepads[0].state["RIGHT_BOTTOM_SHOULDER"];
-}).map(deadZone).throttle(100).distinctUntilChanged(null, sameValue);
+const deadZone = (lower, upper) => x => x < lower ? x : x > upper ? x : 0;
 
-var steering = timer.map(function() {
-  return gamepad.gamepads[0].state["LEFT_STICK_X"];
-}).map(deadZone).throttle(100).distinctUntilChanged(null, sameValue);
+const gamepad = timer.filter(() => gamepadIndex != -1)
 
-var depthThrottle = 0;
-var depth = timer.map(function() {
-  return gamepad.gamepads[0].state["RIGHT_STICK_Y"];
-}).map(deadZone).throttle(100).distinctUntilChanged(null, sameValue);
+const filteredAxis = index => gamepad.map(axis(index)).map(deadZone(-0.1, 0.1)).distinctUntilChanged().throttleTime(10)
 
-throttle.subscribe(function(x) {
-  socket.emit('throttle', x)
-});
+filteredAxis(0).subscribe(axis => {
+  console.log("axis", 0, axis)
+  pos.x = axis * 100;
+  socket.emit('steering', axis * 90 + 90)
+})
 
-steering.subscribe(function(x) {
-  socket.emit('depth', x)
-});
+filteredAxis(1).subscribe(axis => {
+  console.log("axis", 1, axis)
+  pos.y = axis * 100;
+  socket.emit('throttle', -axis * 90 + 90)
+})
 
-depth.subscribe(function(x) {
-  socket.emit('depth', x)
-});
+filteredAxis(2).subscribe(axis => {
+  console.log("axis", 2, axis)
+  pos2.x = axis * 100;
+})
+
+filteredAxis(3).subscribe(axis => {
+  console.log("axis", 3, axis)
+  pos2.y = axis * 100;
+  socket.emit('dive', axis * 90 + 90)
+})
