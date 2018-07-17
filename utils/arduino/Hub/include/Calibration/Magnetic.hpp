bb011c8245daa25a5d07e179d100a614c8752c33
konnor andrews <konnordandrews@gmail.com>
2018-07-16 22:32:37 -0600

    changed magnetic calibration to use class

--- utils/arduino/Hub/include/Calibration/Magnetic.hpp
+++ utils/arduino/Hub/include/Calibration/Magnetic.hpp
@@ -6,57 +6,70 @@
 #include <Components/Sensors/Accelerometer.hpp>
 
 namespace Calibration{
-  namespace Magnetic{
-    struct Model;
-    struct Data;
-
-    void init(Model* model, Data* data);
-    void updateBuffer(Data* data, Components::Sensors::Magnetometer* magnetometer,
-      Components::Sensors::Accelerometer* accelerometer, int count);
-
-    void applyCalibration(Model* model, Components::Sensors::Magnetometer* magnetometer);
-    Model fourElementCalibration(Data data);
-  }
+  class Magnetic;
 }
 
-#define MAGBUFFSIZEX 12	// x dimension in magnetometer buffer (12x24 equals 288 elements)
-#define MAGBUFFSIZEY (2 * MAGBUFFSIZEX)	// y dimension in magnetometer buffer (12x24 equals 288 elements)
+#define VECTOR3 3
 #define X 0
 #define Y 1
 #define Z 2
-#define DEFAULT_GEOMAGNETIC_FIELD 50.0F // (uT)
-#define MAXMEASUREMENTS 240
-#define MESHDELTACOUNTS 50 // magnetic buffer mesh spacing in counts (here 5uT)
 
-struct Calibration::Magnetic::Data{
-  int16_t rawMagnetometerReadings[3][MAGBUFFSIZEX][MAGBUFFSIZEY];
-  int32_t timeIndices[MAGBUFFSIZEX][MAGBUFFSIZEY];
-  int16_t tangentArray[MAGBUFFSIZEX - 1]; // tangents of (100 * angle)
-  int16_t bufferCount;
-};
+#define MATRIX_3X3 3
 
-struct Calibration::Magnetic::Model{
-  float hardIronOffset[3]; // V - x, y, z (uT)
-  float inverseSoftIronMatrix[3][3]; // inv W
-  float geomagneticFieldMagnitude; // B - (uT)
-  float fourBsq; // 4 * B * B - (uT^2)
-  float fitError; // %
+#define MESH_PHI_RESOLUTION 12
+#define MESH_THETA_RESOLUTION (2 * MESH_PHI_RESOLUTION)
+#define MAX_SAMPLES 240 // must be less than or equal to MESH_PHI_RESOLUTION * MESH_THETA_RESOLUTION
 
-  float trialV[3]; // trial V - x, y, z (uT)
-  float trialInvW[3][3]; // trial inv W
-  float trialB; // trial B - (uT)
-  float trialFitError; // %
+#define DEFAULT_GEOMAGNETIC_FIELD 50.0f // (uT)
 
-  float ellipsoidMatrix[3][3]; // A
-  float inverseEllipsoidMatrix[3][3]; // inv A
-  float scratch10x10MatrixA[10][10]; // scratch matrix used in calculations
-  float scratch10x10MatrixB[10][10]; // scratch matrix used in calculations
-  float vector10x1[10]; // scratch vector used in calculations
-  float vector4x1[4]; // scratch vector used in calculations
+#define RAD_DOWN PI * -0.5f
+#define EMPTY_BIN -1
 
-  int calibrationInProgress; // flag
-  int calibrationHasRun; // flag
-  int validCalibration; // flag
+// sensor generates -2048 - 2048 at a gain level
+// use int16_t to minimize size
+
+// this algorithm works best if the sensor outputs data oriented with the
+// z axis pointing up, and the x/y axis level with the ground, x axis pointing forward
+
+class Calibration::Magnetic{
+private:
+  struct Bin{
+    int8_t phi;
+    int8_t theta;
+  };
+
+public:
+  struct Sample{
+    int16_t x;
+    int16_t y;
+    int16_t z;
+  };
+
+  Magnetic();
+  void addSample(Sample sample);
+  void generateModelForOnlyHardIron();
+  Sample transform(Sample sample);
+
+private:
+
+  void precalculateTangents();
+  void initMesh();
+
+  Bin getBinForSample(Sample sample);
+  void insertSample(Sample sample, Bin bin);
+  Bin findOldestSample();
+
+  int16_t sampleMesh[VECTOR3][MESH_PHI_RESOLUTION][MESH_THETA_RESOLUTION];
+  int32_t sampleTimes[MESH_PHI_RESOLUTION][MESH_THETA_RESOLUTION];
+  int16_t sampleCount;
+  int32_t time;
+
+  float hardIronOffset[VECTOR3]; // (uT)
+  float inverseSoftIronMatrix[MATRIX_3X3][MATRIX_3X3];
+  float geomagneticFieldMagnitude; // (uT)
+  float fitError; // (%)
+
+  int16_t tangentBins[MESH_PHI_RESOLUTION - 1];
 };
 
 #endif
